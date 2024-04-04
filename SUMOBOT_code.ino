b4ed7d81cfe08a79b1b669349c40fe205c7c60e2
Linc Bell <129335770+NotAbrahamLincoln@users.noreply.github.com>
2024-04-04 12:42:27 -0400

    Update SUMOBOT_code.ino

--- SUMOBOT_code.ino
+++ SUMOBOT_code.ino
@@ -1,5 +1,8 @@
 int x;
 int y;
+int m;
+int r;
+int z;
 
 void setup() {
   // put your setup code here, to run once:
@@ -7,28 +10,60 @@ void setup() {
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, INPUT);
 
   Serial.begin(9600);
+  digitalWrite(3, LOW);  //full stop
+  digitalWrite(2, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  y = 0;
+  r = 0;
+  tone(7, 98.00);
+  delay(150);
+  tone(7, 130.8);
+  delay(150);
+  tone(7, 164.8);
+  delay(100);
+  tone(7, 196.0);
+  delay(250);
+  noTone(7);
+  delay(100);
+  tone(7, 164.8);
+  delay(100);
+  tone(7, 196.0);
+  delay(600);
+  noTone(7);
+  delay(1000);  //riff
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   x = analogRead(4);
+  m = analogRead(5);
   Serial.println(x);
   delay(100);
   y = "HIGH";
-  if (x > 500) {
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, LOW);
-    Serial.println(x);
- 
-    
-  } else {
+  if ((x < 1000)||/*(m < 1000))*/ {  //makes the bot turn around
     digitalWrite(2, LOW);
     digitalWrite(3, HIGH);
     digitalWrite(4, LOW);
     digitalWrite(5, HIGH);
+    Serial.println(x);
+    delay(700);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    delay(900);
+
+
+
+  } else {
+    digitalWrite(2, HIGH);  //wheels moving forward
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
   };
 }
